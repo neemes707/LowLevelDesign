850a12adc76cad4f22bfe43688187c3a747fb9d5
Neemesh <neemeshkumarsingh@gmail.com>
2025-02-26 19:39:09 +0530

    used volatile to fix reordering and caching bug in singleton pattern

--- DesignPatterns/SingletonDesignPattern/Singleton4.java
+++ DesignPatterns/SingletonDesignPattern/Singleton4.java
@@ -1,8 +1,18 @@
 package LowLevelDesign.DesignPatterns.SingletonDesignPattern;
 // Double-Checked Locking based Java implementation of
 // singleton design pattern
+/*TODO we have use volatile keyword with the Singleton object in order to solve 2 issues
+*  1. Reordering or CPU instructions while creating object which can lead to inconsistency
+*  2. Error while caching (L1 caching) which can again lead to inconsistency
+*   volatile properties ->
+*   ->Volatile objects are always fetched from memory and not from the cache
+*   -> suppose there are instructions 1,2,3 , volatile inst. , 4,5,6
+*     so the reordering of ins. before and after volatile object ins. can happen
+*     and also when volatile ins. is executed all previous instruction are pushed to memory
+* */
+
 public class Singleton4 {
-    private static Singleton4 obj = null;
+    private static volatile Singleton4 obj = null;
     private Singleton4(){
 
     }
