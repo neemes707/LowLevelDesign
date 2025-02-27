78b89241afca50459a680201426374a4b7296d90
Neemesh <neemeshkumarsingh@gmail.com>
2025-02-27 16:08:18 +0530

    command desing pattern code added with example of undo and redo of commands

---
+++ DesignPatterns/CommandDesignPattern/Main.java
@@ -0,0 +1,17 @@
+package LowLevelDesign.DesignPatterns.CommandDesignPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        /*TODO here we are trying to implement undo and redo functionality using command design pattern
+        *  In command design pattern we have 3 layers
+        *  1. Invoker
+        *  2. Command
+        *  3. Receiver*/
+        AirConditioner airConditioner = new AirConditioner();
+        RemoteControl remoteControl = new RemoteControl();
+
+        remoteControl.setCommand(new TurnOnCommand(airConditioner));
+        remoteControl.pressButton();
+        remoteControl.undo();
+    }
+}
